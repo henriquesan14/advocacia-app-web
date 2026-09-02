81c69e76367f51e3c2f77883b84372f6b88a060f
Henrique <henriqueunipe@gmail.com>
2026-09-02 11:14:30 -0300

    ajuste validação de endereço na parte

--- src/app/shared/validators/endereco.validator.ts
+++ src/app/shared/validators/endereco.validator.ts
@@ -2,8 +2,8 @@ import { AbstractControl, ValidatorFn } from "@angular/forms";
 
 export function enderecoValidator(): ValidatorFn {
     return (control: AbstractControl): {[key: string]: any} | null => {
-      const { cep, logradouro, numero, cidade, estado, bairro } = control.value;
-      const hasAtLeastOneField = cep || logradouro || numero || cidade || estado || bairro;
+      const { cep, logradouro, numero, cidade, estado, bairro, complemento } = control.value;
+      const hasAtLeastOneField = cep || logradouro || numero || cidade || estado || bairro || complemento;
       
       // Se pelo menos um campo estiver preenchido, então todos devem estar presentes
       if (hasAtLeastOneField && (!cep || !logradouro || !numero || !cidade || !estado || !bairro)) {
